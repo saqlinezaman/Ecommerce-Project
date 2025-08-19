6bbf7d98ad91e7f720776562bb2875c2e0ec6892
saqlinezaman <saqlinemoaj@gmail.com>
2025-08-20 04:20:05 +0600

    contact massage database create done long way to go

--- sql.sql
+++ sql.sql
@@ -1,10 +1,15 @@
- 
- CREATE TABLE inventory (
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     product_id INT NOT NULL,
-     change_type ENUM('in', 'out') NOT NULL,
-     quantity INT NOT NULL,
-     remark VARCHAR(255) DEFAULT NULL,
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
- );
+-- Active: 1754039895439@@127.0.0.1@3306@ecommerce
+CREATE TABLE IF NOT EXISTS contact_message(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    subject VARCHAR(200) NOT NULL,
+    message TEXT NOT NULL,
+    is_replied INT DEFAULT 0,
+    replied_text TEXT DEFAULT NULL,
+    replied_at DATETIME DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_contact_email ON contact_message(email);
+CREATE INDEX idx_contact_is_replied ON contact_message(is_replied);
